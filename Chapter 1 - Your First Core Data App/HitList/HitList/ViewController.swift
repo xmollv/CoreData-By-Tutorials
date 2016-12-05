75685c31c3a499a1bf69245ece1fb8d5e3dd394e
Xavi Moll <xmollv@gmail.com>
2016-12-05 17:08:39 +0100

    Base project created for the first chapter

---
+++ Chapter 1 - Your First Core Data App/HitList/HitList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Xavi Moll on 05/12/2016.
+//  Copyright © 2016 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
