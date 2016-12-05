a6ff5bcb83a422e62f7f2d34d1a476c617bcb676
Xavi Moll <xmollv@gmail.com>
2016-12-05 18:16:23 +0100

    Finished the first chapter. The app stores and retrieves data from the disk, even though it's done though the AppDelegate which is a terrible code smell

--- Chapter 1 - Your First Core Data App/HitList/HitList/ViewController.swift
+++ Chapter 1 - Your First Core Data App/HitList/HitList/ViewController.swift
@@ -7,17 +7,31 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
-    var names: [String] = []
+
+    var people: [NSManagedObject] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         
         title = "The List"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        do {
+            people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
     }
 
     @IBAction func addName(_ sender: UIBarButtonItem) {
@@ -26,7 +40,7 @@ class ViewController: UIViewController {
         
         let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
             guard let textField = alert.textFields?.first, let nameToSave = textField.text else { return }
-            self.names.append(nameToSave)
+            self.save(name: nameToSave)
             self.tableView.reloadData()
         }
         
@@ -38,16 +52,33 @@ class ViewController: UIViewController {
         
         present(alert, animated: true)
     }
+    
+    func save(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        person.setValue(name, forKey: "name")
+        
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let person = people[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = names[indexPath.row]
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
         return cell
     }
 }
