c5a489938e24c4f276cbf11d84e10f6cf61036ba
Xavi Moll <xmollv@gmail.com>
2016-12-06 12:04:23 +0100

    Reindent using 4 spaces instead of 2

--- Chapter 3 - The Core Data Stack/Dog Walk/ViewController.swift
+++ Chapter 3 - The Core Data Stack/Dog Walk/ViewController.swift
@@ -23,51 +23,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  // MARK: - Properties
-  lazy var dateFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-  }()
-  var walks: [NSDate] = []
-
-  // MARK: - IBOutlets
-  @IBOutlet var tableView: UITableView!
-
-  // MARK: - View Life Cycle
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-  }
+    
+    // MARK: - Properties
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .medium
+        return formatter
+    }()
+    var walks: [NSDate] = []
+    
+    // MARK: - IBOutlets
+    @IBOutlet var tableView: UITableView!
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
 }
 
 // MARK: - IBActions
 extension ViewController {
-
-  @IBAction func add(_ sender: UIBarButtonItem) {
-    walks.append(NSDate())
-    tableView.reloadData()
-  }
+    
+    @IBAction func add(_ sender: UIBarButtonItem) {
+        walks.append(NSDate())
+        tableView.reloadData()
+    }
 }
 
 // MARK: UITableViewDataSource
 extension ViewController: UITableViewDataSource {
-
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return walks.count
-  }
-
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let date = walks[indexPath.row]
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-    cell.textLabel?.text = dateFormatter.string(from: date as Date)
-    return cell
-  }
-
-  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-    return "List of Walks"
-  }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return walks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let date = walks[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = dateFormatter.string(from: date as Date)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "List of Walks"
+    }
 }
