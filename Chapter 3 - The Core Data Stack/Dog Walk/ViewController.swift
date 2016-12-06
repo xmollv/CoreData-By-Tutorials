13d658f3a5be76b6e34472cfa9b944d2bf985919
Xavi Moll <xmollv@gmail.com>
2016-12-06 12:49:32 +0100

    Check if the dog exists, if it isn't, we create it and save it. We also add every walk to the NSOrderedSet of dog walks and save it

--- Chapter 3 - The Core Data Stack/Dog Walk/ViewController.swift
+++ Chapter 3 - The Core Data Stack/Dog Walk/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
         return formatter
     }()
     var managedContext: NSManagedObjectContext!
-    var walks: [NSDate] = []
+    var currentDog: Dog?
     
     // MARK: - IBOutlets
     @IBOutlet var tableView: UITableView!
@@ -43,6 +43,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        let dogName = "Fido"
+        let dogFetch: NSFetchRequest<Dog> = Dog.fetchRequest()
+        dogFetch.predicate = NSPredicate(format: "%K == %@", #keyPath(Dog.name), dogName)
+        
+        do {
+            let results = try managedContext.fetch(dogFetch)
+            if results.count > 0 {
+                //Fido found, use fido
+                currentDog = results.first
+            } else {
+                currentDog = Dog(context: managedContext)
+                currentDog?.name = dogName
+                try managedContext.save()
+            }
+        } catch let error as NSError {
+            print("Fetch error: \(error) description: \(error.userInfo)")
+        }
     }
 }
 
@@ -50,7 +68,24 @@ class ViewController: UIViewController {
 extension ViewController {
     
     @IBAction func add(_ sender: UIBarButtonItem) {
-        walks.append(NSDate())
+        // Insert a new Walk entity into Core Data
+        let walk = Walk(context: managedContext)
+        walk.date = NSDate()
+        
+        // Insert the new Walk into the Dog's walks set
+        if let dog = currentDog, let walks = dog.walks?.mutableCopy() as? NSMutableOrderedSet {
+            walks.add(walk)
+            dog.walks = walks
+        }
+        // The if let statement is the same as calling addToWalks in the currentDog
+        // currentDog?.addToWalks(walk)
+        
+        //Save the managed object context
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Save error: \(error), description: \(error.userInfo)")
+        }
         tableView.reloadData()
     }
 }
@@ -59,13 +94,14 @@ extension ViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let walks = currentDog?.walks else { return 1 }
         return walks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let date = walks[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = dateFormatter.string(from: date as Date)
+        guard let walk = currentDog?.walks?[indexPath.row] as? Walk, let walkDate = walk.date as? Date else { return cell }
+        cell.textLabel?.text = dateFormatter.string(from: walkDate)
         return cell
     }
     
